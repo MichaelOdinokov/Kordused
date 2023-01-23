b44e5da3dae8b6dd38a9e6b47f5cd92f3b2fe1f2
opilane <opilane@HP-CZC2349HTM>
2023-01-23 09:53:24 +0200

    12

--- Kordused/Kordused.py
+++ Kordused/Kordused.py
@@ -1,27 +1,13 @@
 from random import *
 
-#3
-N=randint(1, 10)
-p=0 # p - positive
+#2.1
+n=randint(1, 10)
 
-for i in range(N):
-    number= randint(-10, 10) # generate a random number between -10 and 10
-    if number > 0:
-        p+= 1 
-
-print("Number of positive numbers:", p)
-
-#2
-R=int(input("Enter a number: "))
-n= 1
-for i in range(1, R+1, 2):
-    n*=i
-print(n)
-
-
-#1
-n=int(input("Enter a number (1-9): "))
-p=" *   \n  ***  \n ***** \n*******\n   *" 
 for i in range(n):
-    print(p, end=" ")
+    number=randint(-10, 10) # generate a random number between -10 and 10
+    if number > 0:
 
+
+ (\_/)
+   (o o)
+   / | \*
